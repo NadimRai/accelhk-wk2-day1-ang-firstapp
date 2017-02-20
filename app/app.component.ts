e4eada8974900936165d54d28499cee681c395e2
NadimRai <rai_nadim@yahoo.com>
2017-02-20 11:53:52 -0800

    Loaded Child Components with Separate Files pies

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
+    <h3>Here are my favorite pies!</h3>
+    <pies></pies>
     <div *ngFor="let currentTask of tasks">
       <h3>{{ currentTask.description }}</h3>
       <button (click)="showDetails(currentTask)">Edit</button>
@@ -26,7 +28,8 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public tasks: Task[] = [
+  favoritePies: string[] = ["Apple", "Banana Cream", "Blackberry"];
+  tasks: Task[] = [
       new Task("Create To-Do List app.", 0),
       new Task("Learn Kung Fu.", 1),
       new Task("Rewatch all the Lord of the Rings movies.", 2),
