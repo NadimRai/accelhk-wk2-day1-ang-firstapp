db2e58bb55d6932b651b762ff5c2c101dcfafd2a
NadimRai <rai_nadim@yahoo.com>
2017-02-20 12:22:49 -0800

    Separate out the Edit Tasks Component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { PiesListComponent } from './pies-list.component';
 import { AppComponent }   from './app.component';
+import { PiesListComponent } from './pies-list.component';
+import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule
   ],
-  declarations: [
-    PiesListComponent,
+  declarations: [ 
     AppComponent,
+    PiesListComponent,
+    TaskListComponent,
+    EditTaskComponent,
   ],
   bootstrap:    [ AppComponent ]
 })
